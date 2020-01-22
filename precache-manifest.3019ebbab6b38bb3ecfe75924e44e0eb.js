59baa1c304f7c1c241933d26cc31f0a030c49864
Matt <matt_2020@hotmail.com>
2020-01-22 14:50:38 -0500

    Updates

---
+++ precache-manifest.3019ebbab6b38bb3ecfe75924e44e0eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c55f2cddb80c1ecbbef5d91eee505faa",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2712cec2235520d5fe9f",
+    "url": "/monsters-rolodex/static/css/main.fe082f40.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2712cec2235520d5fe9f",
+    "url": "/monsters-rolodex/static/js/main.d4ea7458.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
